c0370337526b5c242fda23f3f91a681bcbabf5e8
Pan Wangperawong <panthongw@gmail.com>
2015-07-12 21:00:56 -0400

    Added integration with OMDB API and listing movies

---
+++ server.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var app = express();
+var fs = require('fs');
+var path = require('path');
+var bodyParser = require('body-parser');
+
+app.use(express.static(path.join(__dirname, '/public')));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use('/', express.static(path.join(__dirname, 'public')));
+
+app.get('/favorites', function(req, res){
+  var data = fs.readFileSync('./data.json');
+  res.setHeader('Content-Type', 'application/json');
+  res.send(data);
+});
+
+app.post('/favorites', function(req, res){
+  if(!req.body.name || !req.body.oid){
+    res.send("Error");
+    return
+  }
+  var data = JSON.parse(fs.readFileSync('./data.json'));
+  data.push(req.body);
+  fs.writeFile('./data.json', JSON.stringify(data));
+  res.setHeader('Content-Type', 'application/json');
+  res.send(data);
+});
+
+app.set('port', (process.env.PORT || 3000));
+
+app.listen(app.get('port'), function() {
+  console.log('Server started: http://localhost:' + app.get('port') + '/');
+});
